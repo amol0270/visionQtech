5e353516026055f5690724c1309b606c5ba6f685
subhashbunkarjavascripter <subhashbunkarjavascripter9685@gmail.com>
2026-03-02 17:54:40 +0530

    add pic and ad font poppins

--- src/components/ImagesSlider.jsx
+++ src/components/ImagesSlider.jsx
@@ -4,6 +4,8 @@ import slide2 from '../assets/images/slide2.jpg';
 import slide3 from '../assets/images/slide3.jpg';
 import slide4 from '../assets/images/slide4.jpg';
 import slide5 from '../assets/images/slide5.jpg';
+import slide6 from '../assets/images/slide6.jpg';
+import slide7 from '../assets/images/slide7.jpg';
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const slides = [
@@ -29,6 +31,17 @@ const slides = [
   },
   {
     image: slide5,
+    title: <>We Care About <span className="font-semibold">#GEN-NEXT</span></>,
+    description: 'Step to make a better world',
+  },
+  
+  {
+    image: slide6,
+    title: <>Intelligent <span className="font-semibold">#Cyber Security</span> Solution for Every Business Model</>,
+    description: 'Perfect solution backed with AI and ML',
+  },
+  {
+    image: slide7,
     title: <>Intelligent <span className="font-semibold">#Cyber Security</span> Solution for Every Business Model</>,
     description: 'Perfect solution backed with AI and ML',
   },
